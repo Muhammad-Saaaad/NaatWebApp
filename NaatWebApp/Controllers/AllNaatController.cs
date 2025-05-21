09ceeaf9b2b5b9605a1bcdac3e64e9d29ae24239
Muhammad-Saaad <javed.iqbal12458@gmail.com>
2025-05-21 14:44:15 +0500

    Added Naat controller, naat view, naat model and corrected the table view from show albums

---
+++ NaatWebApp/Controllers/AllNaatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NaatWebApp.Controllers
+{
+    public class AllNaatController : Controller // table name Naats
+    {
+        public ActionResult AllNaat(String nid, int ano, String title)
+        {
+            return View();
+        }
+
+        public ActionResult CreateNaat()
+        {
+            return View();
+        }
+    }
+}
---
+++ NaatWebApp/Models/Naat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NaatWebApp.Models
+{
+    public class Naat
+    {
+        public String nid { get; set; }
+        public int ano { get; set; }
+        public int nno { get; set; }
+        public String naat { get; set; }
+        public String filepath { get; set; }
+        public HttpPostedFileBase audiofile { get; set; }
+    }
+}
